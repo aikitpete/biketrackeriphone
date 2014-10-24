167436b23b26154638a1649994bc0f1120d2dc2e
Peter Gerhat <swyna@swynasharing.local>
2014-10-24 23:55:37 +0200

    Initial Commit

---
+++ BikeTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BikeTracker
+//
+//  Created by Peter Gerhat on 10/24/14.
+//  Copyright (c) 2014 Peter Gerhat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BikeTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BikeTracker
+//
+//  Created by Peter Gerhat on 10/24/14.
+//  Copyright (c) 2014 Peter Gerhat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
